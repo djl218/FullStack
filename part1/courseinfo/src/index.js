3f745dda28049405a2da9021f6634a47395f489d
Daniel Leskosky <dleskosky@gmail.com>
2020-05-29 21:55:56 -0400

    Refactored commit for exercise 1.3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,41 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-  return <p>{props.part} {props.exercises}</p>
-}
-
-const Content = (props) => {
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  return (
-    <div>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-}
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = {
@@ -49,6 +14,29 @@ const App = () => {
   const part3 = {
     name: 'State of a component',
     exercises: 14
+  }
+
+  const Header = (props) => {
+    return <h1>{props.course}</h1>
+  }
+  
+  const Part = (props) => {
+    return <p>{props.part} {props.exercises}</p>
+  }
+  
+  const Content = (props) => {
+   
+    return (
+      <div>
+        <Part part={part1.name} exercises={part1.exercises} />
+        <Part part={part2.name} exercises={part2.exercises} />
+        <Part part={part3.name} exercises={part3.exercises} />
+      </div>
+    )
+  }
+  
+  const Total = (props) => {
+    return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   }
  
   return (
