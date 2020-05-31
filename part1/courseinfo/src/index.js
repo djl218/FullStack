c6150c436950a9d193e40c6417d1ad2d31b0dc21
Daniel Leskosky <dleskosky@gmail.com>
2020-05-31 13:58:49 -0400

    Refactored commit of exercise 1.5.  The Header, Content, and Total components are no longer defined inside of the App component.

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+
+} 
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -20,25 +39,6 @@ const App = () => {
     ]
   }
 
-  const Header = (props) => {
-    return <h1>{props.course.name}</h1>
-  }
-
-  const Content = (props) => {
-    return (
-      <div>
-        <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-        <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-        <p>{props.parts[2].name} {props.parts[2].exercises}</p>
-      </div>
-    )
-  }
-  
-  const Total = (props) => {
-    return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
- 
-  } 
- 
   return (
     <div>
       <Header course={course} />
