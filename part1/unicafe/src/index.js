4e951d1e5923d056e3f024d1f3502cbb8f119e87
Daniel Leskosky <dleskosky@gmail.com>
2020-06-06 15:33:47 -0400

    Commit for exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -14,11 +14,29 @@ const Button = (props) => {
   )
 }
 
-const Statistics = (props) => {
-  const { text, allClicks} = props
+const Counter = (props) => {
+  const { text, allClicks } = props
   return (
     <div>
       {text} {allClicks}
+    </div>
+  )
+}
+
+const Average = (props) => {
+  const { text, allClicks } = props
+  return (
+    <div>
+      {text} {allClicks}
+    </div>
+  )
+}
+
+const PercentPositive = (props) => {
+  const { text, allClicks } = props
+  return (
+    <div>
+      {text} {allClicks} %
     </div>
   )
 }
@@ -27,17 +45,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const [averageGood, setAverageGood] = useState(0)
+  const [averageNeutral, setAverageNeutral] = useState(0)
+  const [averageBad, setAverageBad] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAverageGood(averageGood + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAverageNeutral(averageNeutral + 0)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setAverageBad(averageBad - 1)
   }
 
   return (
@@ -47,9 +72,12 @@ const App = () => {
       <Button onClick={handleNeutral} text='neutral' />
       <Button onClick={handleBad} text='bad' />
       <Header header='statistics' />
-      <Statistics text='good' allClicks={good} />
-      <Statistics text='neutral' allClicks={neutral} />
-      <Statistics text='bad' allClicks={bad} />
+      <Counter text='good' allClicks={good} />
+      <Counter text='neutral' allClicks={neutral} />
+      <Counter text='bad' allClicks={bad} />
+      <Counter text='all' allClicks={good + neutral + bad} />
+      <Average text='average' allClicks={(averageGood + averageNeutral + averageBad) / (good + neutral + bad)} />
+      <PercentPositive text='positive' allClicks={good / (good + neutral + bad) * 100} />
     </div>
   )
 }
