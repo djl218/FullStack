404079e2df4ec66093cefecc74423a7916e8dcdf
Daniel Leskosky <dleskosky@gmail.com>
2020-06-06 20:25:21 -0400

    Cleaner commit for exercise 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -19,7 +19,7 @@ const Statistic = (props) => {
   const { text, allClicks, text2 } = props
   return (
     <div>
-      {text} {allClicks} {text2}             
+     {text} {allClicks} {text2} 
     </div>
   )
 }
@@ -28,24 +28,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  const [averageGood, setAverageGood] = useState(0)
-  const [averageNeutral, setAverageNeutral] = useState(0)
-  const [averageBad, setAverageBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
-    setAverageGood(averageGood + 1)
+    setAll(all + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
-    setAverageNeutral(averageNeutral + 0)
+    setAll(all + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
-    setAverageBad(averageBad - 1)
+    setAll(all + 1)
   }
 
   if (good + neutral + bad === 0) {
@@ -63,18 +60,18 @@ const App = () => {
 
   return (
     <div>
-        <Header header='give feedback' />
-        <Button onClick={handleGood} text='good' />
-        <Button onClick={handleNeutral} text='neutral' />
-        <Button onClick={handleBad} text='bad' />
-        <Header header='statistics' />
-        <Statistic text='good' allClicks={good} />
-        <Statistic text='neutral' allClicks={neutral} />
-        <Statistic text='bad' allClicks={bad} />
-        <Statistic text='all' allClicks={good + neutral + bad} />
-        <Statistic text='average' allClicks={(averageGood + averageNeutral + averageBad) / (good + neutral + bad)} />
-        <Statistic text='positive' allClicks={good / (good + neutral + bad) * 100} text2={'%'} />
-    </div>
+      <Header header='give feedback' />
+      <Button onClick={handleGood} text='good' />
+      <Button onClick={handleNeutral} text='neutral' />
+      <Button onClick={handleBad} text='bad' />
+      <Header header='statistics' />
+      <Statistic text='good' allClicks={good} />
+      <Statistic text='neutral' allClicks={neutral} />
+      <Statistic text='bad' allClicks={bad} />
+      <Statistic text='all' allClicks={all} />
+      <Statistic text='average' allClicks={((good * 1) + (neutral * 0) + (bad * -1)) / all} />
+      <Statistic text='positive' allClicks={good / all * 100} text2={'%'} />      
+    </div>    
   )
 }
 
