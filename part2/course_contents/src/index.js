0286503a0a5148cd843cca8d16a55e7c8d88ff96
Daniel Leskosky <dleskosky@gmail.com>
2020-06-13 15:40:09 -0400

    Commit for exercises 2.2 & 2.3

--- part2/course_contents/src/index.js
+++ part2/course_contents/src/index.js
@@ -14,6 +14,15 @@ const Course = (props) => {
   )
 }
 
+const Total = props => {
+  const total = props.course.parts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.exercises,
+    0
+  );
+  return <b>total of {total} exercises</b>;
+};
+
+
 const App = () => {
   const course = {
     id: 1,
@@ -33,11 +42,21 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
 
-  return <Course course={course} />
+  return (
+    <div>
+      <Course course={course} />
+      <Total course={course} />
+    </div>
+  )  
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
