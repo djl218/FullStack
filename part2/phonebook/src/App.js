ac922f8f9f50962bb3459ff0634f00c1e47745f0
Daniel Leskosky <dleskosky@gmail.com>
2020-06-14 13:03:51 -0400

    Commit for phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,11 @@
 import React, { useState } from 'react'
+//import Person from './components/Person'
+
+const Person = (props) => {
+  return (
+      <div>{props.person.name}</div>
+  )
+}
 
 const App = () => {
   const [ persons, setPersons ] = useState([
@@ -6,19 +13,36 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleNewName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      content: newName,
+      date: new Date().toISOString(),
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input />
-        </div>
+        <div onSubmit={addPerson}>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>  
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <Person key={persons.name} person={person} />
+      )}      
     </div>
   )
 }
