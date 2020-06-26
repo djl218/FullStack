a1ebb829431de6a5a8170f88f33aac851b59ef8b
Daniel Leskosky <dleskosky@gmail.com>
2020-06-25 20:38:48 -0400

    Commit for exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = 
-    useState([
-      { name: 'Arto Hellas', number: '040-123456' },
-      { name: 'Ada Lovelace', number: '39-44-5323523'},
-      { name: 'Dan Abramov', number: '12-43-234345'},
-      { name: 'Mary Poppendieck', number: '39-23-6423122'}
-    ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newSearch, setNewSearch ] = useState('')
   const [ searchResults, setSearchResults ] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNewSearch = (event) => {
     console.log(event.target.value)
@@ -31,7 +36,7 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     const personSearch = 
       persons.filter(person => person.name.toLowerCase().includes(newSearch)
       )   
